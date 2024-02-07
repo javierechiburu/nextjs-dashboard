ee7436230b48c86aaadcb89d809f24eb368004b6
javierechiburu <javierechiburu.g@gmail.com>
2024-02-07 14:57:53 -0300

    node version

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,9 @@
-import { Inter, Montserrat } from 'next/font/google';
+import { Inter, Montserrat, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const montserrat = Montserrat({ subsets: ['latin'] });
+ 
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
